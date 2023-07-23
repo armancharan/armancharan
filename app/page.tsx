eb7568826fb7097728fa522a00426a6581909a34
Arman Charan <armancharan@armancharan.com>
2023-07-23 11:21:18 +1000

    Mix up the fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,59 @@
+import { Merriweather, Roboto } from 'next/font/google'
+import localFont from 'next/font/local'
+import { twMerge } from 'tailwind-merge'
 import { entries } from '../blog/entries'
 import { BlogEntryList } from '../ui/blog_entry_list'
 import { Page } from '../ui/page'
+
+const caprasimo = localFont({
+  src: '../public/fonts/Caprasimo-Regular.ttf',
+  // style: 'normal',
+})
+
+const merriweather = Merriweather({
+  weight: '700',
+  subsets: ['latin'],
+  style: 'italic',
+})
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '500',
+  style: 'italic',
+})
 
 const HomePage = () => {
   return (
     <Page>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
-        "Oh, bass," <img className="max-h-full  m-4 inline h-16" src={'/ivy-smiling.png'} /> cool
+        <em className={merriweather.className}>"Oh, bass,"</em>{' '}
+        <Image className={'h-16'} src={'/ivy-smiling.png'} />{' '}
+        <em className={roboto.className}>cool</em>
       </h1>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
         You know how you
-        <img className="max-h-full m-4 inline h-24" src={'/pesto-pasta.png'} />{' '}
-        make the bass better?
+        <Image src={'/pesto-pasta.png'} />{' '}
+        <em className={caprasimo.className} style={{ letterSpacing: '0.5px' }}>make</em> the{' '}
+        <em className={caprasimo.className} style={{ letterSpacing: '0.5px' }}>bass</em> better?
       </h1>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
-        Crank<img className="max-h-full m-4 inline h-20" src={'/featherston-numero-iv.png'} />{' '}
-        the bass up
+        <u>Crank</u> <Image src={'/featherston-numero-iv.png'} /> the <u>bass</u> <b>up</b>!
       </h1>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
-        (Yeah) <img className="max-h-full m-4 inline h-20" src={'/polly.png'} />
+        <em className={merriweather.className}>(Yeah!)</em> <Image src={'/polly.png'} />
       </h1>
     </Page>
   )
 }
 
 export default HomePage
+
+const Image: React.ComponentType<{
+  className?: string
+  src: string
+}> = ({
+  className,
+  src,
+}) => {
+  return <img className={twMerge('max-h-full m-4 inline h-20', className)} src={src} />
+}
