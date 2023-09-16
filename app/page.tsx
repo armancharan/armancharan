b9f0a070eaa2fe40fd28c52456f1b16ba3832477
Arman Charan <armancharan@armancharan.com>
2023-09-16 11:29:09 +1000

    Do less

--- app/page.tsx
+++ app/page.tsx
@@ -20,21 +20,18 @@ const HomePage = () => {
   return (
     <Page>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
-        <em className={merriweather.className}>"Oh, bass, cool"</em>{' '}
+        "Oh, bass, cool"
         <Image className={'h-16'} src={'/ivy-smiling.png'} />
       </h1>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
         You know how you
-        <Image src={'/pesto-pasta.png'} />{' '}
-        <em className={caprasimo.className} style={{ letterSpacing: '0.5px' }}>make</em> the{' '}
-        <em className={caprasimo.className} style={{ letterSpacing: '0.5px' }}>bass</em>{' '}
-        <b>better</b>?
+        <Image src={'/pesto-pasta.png'} /> make the bass better?
       </h1>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
-        <u>Crank</u> <Image src={'/featherston-numero-iv.png'} /> the <u>bass</u> <b>up</b>
+        Crank <Image src={'/featherston-numero-iv.png'} /> the bass up
       </h1>
       <h1 className="h-30 line leading-[60px] text-5xl font-medium italic text-white">
-        <em className={merriweather.className}>(Yeah)</em> <Image src={'/polly.png'} />
+        (Yeah) <Image src={'/polly.png'} />
       </h1>
     </Page>
   )
