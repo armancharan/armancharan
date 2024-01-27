88e0c674e76d22cfd0889a31419588219a9edcfe
Arman Charan <armancharan@armancharan.com>
2024-01-27 23:18:14 +1000

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import { PropsWithChildren } from 'react'
-import { NavigationBar } from '../ui/navigation_bar'
+import { Analytics } from "@vercel/analytics/react";
+import { PropsWithChildren } from "react";
+import { NavigationBar } from "../ui/navigation_bar";
 
-import { Metadata } from 'next'
-import Head from 'next/head'
-import { Footer } from '../ui/footer'
-import './global.css'
+import { Metadata } from "next";
+import Head from "next/head";
+import { Footer } from "../ui/footer";
+import "./global.css";
 
 export const metadata: Metadata = {
-  manifest: '/manifest.json',
-  title: 'armancharan.com',
-  themeColor: '#000000',
-}
+  manifest: "/manifest.json",
+  title: "armancharan.com",
+  themeColor: "#000000",
+};
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
@@ -22,9 +23,10 @@ const RootLayout = ({ children }: PropsWithChildren) => {
         <NavigationBar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
